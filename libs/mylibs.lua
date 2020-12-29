b390dab5f94b9bb1e780a65c52625a9a89b3cbb3
lima <lima@mbp>
2020-12-29 13:46:56 -0600

    updates

--- libs/mylibs.lua
+++ libs/mylibs.lua
@@ -1,11 +1,11 @@
 local function table_copy(t)
   -- copy a table (deep copy)
   local copy = {}
-  for n,v in pairs(t) do
+  for k,v in pairs(t) do
     if type(v) == 'table' then
       v = table_copy(v)
     end
-    copy[n] = v
+    copy[k] = v
   end
   return copy
 end
