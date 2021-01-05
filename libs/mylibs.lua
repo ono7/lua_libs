86f53c2e29e478eaf38d627019cb3682a95aebf0
lima <lima@mbp>
2021-01-05 11:49:36 -0600

    adding lua hex/str functions

--- libs/mylibs.lua
+++ libs/mylibs.lua
@@ -1,3 +1,7 @@
+---------------------------------------------------------------------------------
+--                              table operations                               --
+---------------------------------------------------------------------------------
+
 local function table_copy(t)
   -- copy a table (deep copy)
   local copy = {}
@@ -9,6 +13,10 @@ local function table_copy(t)
   end
   return copy
 end
+
+---------------------------------------------------------------------------------
+--                            iterate over strings                             --
+---------------------------------------------------------------------------------
 
 --- iterate over strings using string.byte
 
@@ -38,3 +46,74 @@ for c in str:gmatch "." do
   -- do something with c
   print(c)
 end
+
+---------------------------------------------------------------------------------
+--                             tostring metamethod                             --
+---------------------------------------------------------------------------------
+
+-- prints table to stdout, affects tostring and print functions
+function mt.__tostring(tbl)
+  -- mt = metatable
+  local result = "{"
+
+  for i = 1, #tbl do
+    if i > 1 then
+      result = result .. ", "
+    end
+    result = result .. tostring(tbl[i])
+  end
+
+  result = result .. "}"
+  return result
+end
+
+---------------------------------------------------------------------------------
+--                                  __concat                                   --
+---------------------------------------------------------------------------------
+
+-- mt.__contact
+
+mt.__concat = mt.__add
+
+print(t1 .. t2)
+
+-- mt.__newindex
+
+function mt.___newindex(tbl, key, value)
+  if key == "banana" then
+    error("Cannot set a protected key")
+  else
+    rawset(tbl, key, value)
+  end
+end
+
+---------------------------------------------------------------------------------
+--                                     hex                                     --
+---------------------------------------------------------------------------------
+
+local string = require "string"
+
+function string.fromhex(str)
+  return (str:gsub(
+    "..",
+    function(cc)
+      return string.char(tonumber(cc, 16))
+    end
+  ))
+end
+
+function string.tohex(str)
+  return (str:gsub(
+    ".",
+    function(c)
+      return string.format("\\x%02X", string.byte(c))
+    end
+  ))
+end
+
+--[[
+
+> string.tohex('AAAA')
+\x41\x41\x41\x41
+
+--]]
